8450c47df873b51e384f268ead5a516763177374
josh <josh@bitperfect.at>
2025-06-28 08:07:50 +0200

    Fix players can no longer change fear

--- src/components/gmdaggerheart/Fear.tsx
+++ src/components/gmdaggerheart/Fear.tsx
@@ -3,8 +3,10 @@ import { useShallow } from "zustand/react/shallow";
 import styles from "./fear.module.scss";
 import { DiamondSvg } from "../svgs/DiamondSvg.tsx";
 import { updateRoomMetadata } from "../../helper/helpers.ts";
+import { usePlayerContext } from "../../context/PlayerContext.ts";
 
 export const Fear = () => {
+    const playerContext = usePlayerContext();
     const room = useMetadataContext(useShallow((state) => state.room));
 
     return (
@@ -15,7 +17,9 @@ export const Fear = () => {
                         <DiamondSvg
                             key={index}
                             onClick={async () => {
-                                await updateRoomMetadata(room, { fear: Math.max((room?.fear || 0) - 1, 0) });
+                                if (playerContext.role === "GM") {
+                                    await updateRoomMetadata(room, { fear: Math.max((room?.fear || 0) - 1, 0) });
+                                }
                             }}
                             onContextMenu={() => {}}
                         />
@@ -28,7 +32,9 @@ export const Fear = () => {
                         <DiamondSvg
                             key={index}
                             onClick={async () => {
-                                await updateRoomMetadata(room, { fear: Math.min((room?.fear || 0) + 1, 12) });
+                                if (playerContext.role === "GM") {
+                                    await updateRoomMetadata(room, { fear: Math.min((room?.fear || 0) + 1, 12) });
+                                }
                             }}
                             onContextMenu={() => {}}
                         />
